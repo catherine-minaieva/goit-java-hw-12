8789413b4c7519a2b48212381e0cfa95e5bfe67f
catherine-minaieva <75452152+catherine-minaieva@users.noreply.github.com>
2021-05-15 19:42:31 +0300

    Task 12-2

--- Task2/TestMain.java
+++ Task2/TestMain.java
@@ -2,7 +2,7 @@ package HomeWork12.Task2;
 
 public class TestMain {
     public static void main(String[] args) {
-        MyExecutor ex = new MyExecutor(3);
+        MyExecutor ex = new MyExecutor(10);
         ex.execute(new MyRunnable());
         ex.shutdown();
     }
